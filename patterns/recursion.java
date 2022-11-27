983f8d45f657a6453f5fa629ee8f37ccc3916797
adarsh-shahi <adarshshahi1009@gmail.com>
2022-11-27 15:19:42 +0530

    first subsequence

--- patterns/recursion.java
+++ patterns/recursion.java
@@ -3,13 +3,13 @@ package patterns;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class recursion{
+public class recursion {
     public static void main(String[] args) {
-        subsequences(new int[] {1,2,3}, 0, new ArrayList<>());
+        printFirstSubsequence(new int[]{1, 2, 3, 2}, 0, new ArrayList<>(), 3,0);
     }
 
-    static void subsequences(int[] array, int currentIndex, ArrayList<Integer> ans){
-        if(currentIndex == array.length) {
+    static void subsequences(int[] array, int currentIndex, ArrayList<Integer> ans) {
+        if (currentIndex == array.length) {
             System.out.println(ans);
             return;
         }
@@ -20,4 +20,35 @@ public class recursion{
         subsequences(array, currentIndex + 1, ans);
 
     }
+    static boolean printFirstSubsequence(int[] array, int currentIndex, ArrayList<Integer> ans, int k, int sum) {
+        if (currentIndex == array.length) {
+            if (sum == k) {
+                for (int e : ans) System.out.print(e + " ");
+                return true;
+            }
+            return false;
+        }
+
+        ans.add(array[currentIndex]);
+        if (printFirstSubsequence(array, currentIndex + 1, ans, k, sum + array[currentIndex])) return true;
+        ans.remove(ans.size() - 1);
+        if (printFirstSubsequence(array, currentIndex + 1, ans, k, sum)) return true;
+        return false;
+    }
+
+//    static void subsequencesEqualToSum(int[] array, int sum, int k, int i, ArrayList<Integer> ans) {
+//        if (sum == k) {
+//            System.out.println(ans);
+//            return;
+//        }
+//        if (i >= array.length) return;
+//        if (sum + array[i] <= k) {
+//            ans.add(array[i]);
+//            subsequencesEqualToSum(array, sum + array[i], k, i + 1, ans);
+//        } else {
+//            subsequencesEqualToSum(array, sum, k, i + 1, ans);
+//        }
+//        ans.remove(ans.size() - 1);
+//        subsequencesEqualToSum(array, sum, k, i + 1, ans);
+//    }
 }
